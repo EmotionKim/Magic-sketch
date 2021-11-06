8db7dcd65c06443d7e88dee72988f71583d28610
boyuuuun <boyoon54@gmail.com>
2021-11-07 00:35:18 +0900

    color to grey 변환 완료

--- SPADE/color_to_grey.py
+++ SPADE/color_to_grey.py
@@ -17,3 +17,18 @@ def convert_rgb_image_to_greyscale(input_file, output_file):
 
             label_to_rgb[label] = rgb
             rgb_to_label[rgb] = label
+            label_to_grey[label] = int(grey) - 1
+
+    in_img = Image.open(input_file)
+    out_img = Image.new("L", (in_img.size[0], in_img.size[1]))
+    pixels = in_img.load()
+    p_o = out_img.load()
+    grey = (0)
+
+    for i in range(in_img.size[0]):
+        for j in range(in_img.size[1]):
+            if(pixels[i, j][0:3] in rgb_to_label.keys()):
+                label = rgb_to_label[pixels[i, j][0:3]]
+                grey = label_to_grey[label]
+            p_o[i, j] = grey
+    # TODO: 파일 저장
