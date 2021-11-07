dec1fe253850d7368a607801911b1aff4f1a0197
sukyung99 <201802067@o.cnu.ac.kr>
2021-11-08 01:59:03 +0900

    모델 run 추가 (test.py)

--- SPADE/test.py
+++ SPADE/test.py
@@ -3,43 +3,35 @@ Copyright (C) 2019 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 """
 
-import os
-from collections import OrderedDict
+import importlib
 
-import data
-from options.test_options import TestOptions
 from models.pix2pix_model import Pix2PixModel
-from util.visualizer import Visualizer
-from util import html
+from options.test_options import TestOptions
 
-opt = TestOptions().parse()
 
-dataloader = data.create_dataloader(opt)
+def run(verbose=False):
+    opt = TestOptions().parse(verbose = verbose)
 
-model = Pix2PixModel(opt)
-model.eval()
+    dataset_name = "coco"
+    dataset_filename = "data." + dataset_name + "_dataset"
 
-visualizer = Visualizer(opt)
+    datasetlib = importlib.import_module(dataset_filename)
 
-# create a webpage that summarizes the all results
-web_dir = os.path.join(opt.results_dir, opt.name,
-                       '%s_%s' % (opt.phase, opt.which_epoch))
-webpage = html.HTML(web_dir,
-                    'Experiment = %s, Phase = %s, Epoch = %s' %
-                    (opt.name, opt.phase, opt.which_epoch))
+    dataset = None
+    target_dataset_name = dataset_name.replace('_', '') + 'dataset'
 
-# test
-for i, data_i in enumerate(dataloader):
-    if i * opt.batchSize >= opt.how_many:
-        break
+    # TODO: dataset 이름 정렬
 
-    generated = model(data_i, mode='inference')
+    model = Pix2PixModel(opt, verbose)
+    model.eval()
 
-    img_path = data_i['path']
-    for b in range(generated.shape[0]):
-        print('process image... %s' % img_path[b])
-        visuals = OrderedDict([('input_label', data_i['label'][b]),
-                               ('synthesized_image', generated[b])])
-        visualizer.save_images(webpage, visuals, img_path[b:b + 1])
+    if verbose:
+        print(dataloader)
+    for i, data_i in enumerate(dataloader):
+        if i * opt.batchSize >= opt.how_many:
+            break
+        model(data_i, mode='inference', verbose=verbose)
 
-webpage.save()
+
+if __name__ == "__main__":
+    run()
